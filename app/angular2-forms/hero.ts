dc1059aadd3d393eee68f69bc247de96eebc47e2
Gxerxes <xuninglee@163.com>
2016-04-29 08:29:06 +0800

    add hero
    add hero

---
+++ app/angular2-forms/hero.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by LeonL64 on 4/14/2016.
+ */
+export class Hero {
+    constructor(
+        public id: number,
+        public name: string,
+        public power: string,
+        public alias?: string
+    ){ }
+}
